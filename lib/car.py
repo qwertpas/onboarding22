61257fdbd8c3ced138c20416afbf11e1b9be940b
Christopher Xu <cyx3@illinois.edu>
2022-08-11 11:12:13 -0700

    pandas and carpropr

--- lib/car.py
+++ lib/car.py
@@ -1,8 +1,25 @@
-
+from ast import main
+import json
+import pandas as pd
 
 class Car:
-    def __init__(self, name):
-        self.name = name
 
-    def get_name(self):
-        return self.name
+    def __init__(self, car_name):
+        self.car_name = car_name
+
+        with open(f"./lib/cars/{car_name}.json", 'r') as props_json:
+            self.props = json.load(props_json)
+        
+
+    def get_drag_coeff(self):
+        return self.props['drag_coeff']
+
+
+
+
+def main():
+    car = Car('brizo')
+    print(car.get_drag_coeff())
+
+if __name__ == "__main__":
+    main()
